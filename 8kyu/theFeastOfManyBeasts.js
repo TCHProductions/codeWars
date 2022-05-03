bcc1b8cdd13da1d04231b035744a85d7d62af831
TCHProductions <102759638+TCHProductions@users.noreply.github.com>
2022-05-03 17:09:55 +1000

    Create theFeastOfManyBeasts.js

---
+++ 8kyu/theFeastOfManyBeasts.js
@@ -0,0 +1,9 @@
+function feast(beast, dish) {
+    //your function here
+      if(beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1]){
+        return true
+      }
+      else{
+        return false
+      }
+    }
