95fcc58060b3903ee8eae3c19eef4f74f3222e82
TCHProductions <102759638+TCHProductions@users.noreply.github.com>
2022-07-05 20:42:49 +1000

    Update trainingJsSevenIfElseAndTernaryOperater.js

--- 8kyu/trainingJsSevenIfElseAndTernaryOperater.js
+++ 8kyu/trainingJsSevenIfElseAndTernaryOperater.js
@@ -27,6 +27,7 @@ Condition and statement separated by "?", different statement separated by ":" i
 function oddEven(n){
   return n%2==1 ? "odd number" : "even number";
 }
+function oldYoung(age){
   return age<16 ? "children" : age<50 ? "young man" : "old man";
 }
 Task:
